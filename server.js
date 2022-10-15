a957d9372d6d156db60f67f3dfd4c8b3cb67342d
Stefan Joebstl <stefanjb76@gmail.com>
2022-10-15 10:31:22 +0200

    First init

---
+++ server.js
@@ -0,0 +1,19 @@
+const createHafas = require('hafas-client')
+const oebbProfile = require('hafas-client/p/oebb')
+const createApi = require('hafas-rest-api')
+
+const config = {
+	hostname: 'STVL2',
+	name: 'elcapo-paris',
+	homepage: 'https://github.com/stefanjb-it/elcapo-paris',
+	version: '1.0.0',
+	aboutPage: false
+}
+
+const hafas = createHafas(oebbProfile, 'elcapo-paris')
+
+const api = createApi(hafas, config)
+
+api.listen(8080, (err) => {
+	if (err) console.error(err)
+})
