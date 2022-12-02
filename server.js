b09fa7d43d749fb49d705200cc172ee5a271c144
stefanjb-it <stefanjb76@gmail.com>
2022-12-02 14:21:09 +0100

    Changed name

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const oebbProfile = require('hafas-client/p/oebb')
 const createApi = require('hafas-rest-api')
 
 const config = {
-	hostname: 'hafas-rest-service',
-	name: 'elcapo-paris',
-	homepage: 'https://github.com/stefanjb-it/elcapo-paris',
+	hostname: 'oebb-hafas',
+	name: 'oebb-hafas',
+	homepage: 'https://github.com/stefanjb-it/Root-Navigator',
 	version: '1.0.0',
 	aboutPage: false
 }
 
-const hafas = createHafas(oebbProfile, 'elcapo-paris')
+const hafas = createHafas(oebbProfile, 'oebb-hafas')
 
 const api = createApi(hafas, config)
 
