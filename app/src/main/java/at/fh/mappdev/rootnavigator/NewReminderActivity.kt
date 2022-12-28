5f37c2922d40881923bd2d916c64a31f81d85b48
einfachhuba <huba.csicsics@edu.fh-joanneum.at>
2022-12-28 22:32:36 +0100

    New Reminder Screen

--- app/src/main/java/at/fh/mappdev/rootnavigator/NewReminderActivity.kt
+++ app/src/main/java/at/fh/mappdev/rootnavigator/NewReminderActivity.kt
@@ -1,26 +1,24 @@
 package at.fh.mappdev.rootnavigator
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
@@ -28,23 +26,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import at.fh.mappdev.rootnavigator.ui.theme.RootNavigatorTheme
+import java.util.*
 
-class NewReminderActivity : ComponentActivity() {
-
-    private val selectedPictureLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
-
-    }
+object NewReminderActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        actionBar?.hide()
-
         setContent {
             RootNavigatorTheme {
-                Surface(
-
-                ) {
+                Surface( ) {
                     NewReminderUI()
                 }
             }
@@ -52,10 +42,36 @@ class NewReminderActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
-fun NewReminderUI(){
-    // Wrapper Element für Background Color!
+fun NewReminderUI(context: Context = LocalContext.current){
+
+    var date by remember { mutableStateOf("") }
+    var time by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+    val hour = calendar.get(Calendar.HOUR)
+    val minute = calendar.get(Calendar.MINUTE)
+
+    calendar.time = Date()
+
+    val datePickerDialog = DatePickerDialog (
+        context,
+        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            date = "$dayOfMonth/${month+1}/$year"
+        }, year, month, day
+    )
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _: TimePicker, hour: Int, minute: Int ->
+            time = "${if (hour <10) { "0" + hour} else {hour}}:${if (minute <10) { "0" + minute} else {minute}}"
+        }, hour, minute, true
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -65,5 +81,120 @@ fun NewReminderUI(){
                 contentScale = ContentScale.FillWidth
             )
     ) {
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, top = 32.dp,  end = 32.dp),
+            horizontalArrangement = Arrangement.Center
+
+        ) {
+            Button(onClick = { datePickerDialog.show() },
+                modifier = Modifier
+                    .width(width = 150.dp)
+                    .height(height = 35.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+            ) {
+                Text(
+                    text = "Pick Date",
+                    color = MaterialTheme.colors.surface,
+                    fontSize = 14.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(onClick = { timePickerDialog.show() },
+                modifier = Modifier
+                    .width(width = 150.dp)
+                    .height(height = 35.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+            ) {
+                Text(
+                    text = "Pick Time",
+                    color = MaterialTheme.colors.surface,
+                    fontSize = 14.sp
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(top = 12.dp))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, end = 32.dp),
+            horizontalArrangement = Arrangement.Center
+
+        ) {
+            Text(
+                text = date,
+                color = MaterialTheme.colors.surface,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = time,
+                color = MaterialTheme.colors.surface,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(top = 12.dp))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, end = 32.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = description,
+                onValueChange = { description = it },
+                modifier = Modifier
+                    .height(height = 150.dp)
+                    .fillMaxWidth(),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = MaterialTheme.colors.secondaryVariant
+                ),
+                textStyle = TextStyle(
+                    fontFamily = FontFamily.SansSerif,
+                    color = MaterialTheme.colors.surface,
+                    fontSize = 14.sp
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(top = 12.dp))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, end = 32.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO Implement back to ReminderOverview*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(height = 50.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.button_save),
+                    color = MaterialTheme.colors.surface,
+                    fontSize = 18.sp
+                )
+            }
+        }
     }
 }
+
+
+
+
+
+
